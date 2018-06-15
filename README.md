658dfe4c106984df2385d4987d79d8a8ae0ef129
Edmond <Edmond.Guan@adp.com>
2018-06-15 15:23:53 +0800

    initial project

---
+++ README.md
@@ -0,0 +1,7 @@
+## Full Stack demo by React and Nodejs
+This is backend
+
+> **FrontEnd: React + Redux + Antd + stylus + webpack**
+
+> **BackEnd: Node + koa + sequelize + jwt**
+
---
+++ app.js
@@ -0,0 +1,36 @@
+import Koa from 'koa'
+import http from 'http'
+import convert from 'koa-convert'
+import logger from 'koa-logger'
+import cors from 'koa-cors'
+import bodyParser from 'koa-bodyparser'
+import onerror from 'koa-onerror'
+import resource from 'koa-static'
+import path from 'path'
+
+let app = new Koa()
+
+onerror(app)
+
+app.proxy = true
+
+app
+	.use(convert(cors()))
+	.use(convert(logger()))
+	.use(bodyParser())
+	.use(async (ctx, next) => {
+		await next()
+		ctx.set('X-Powered-By', 'Koa2')
+	})
+	.use(async (ctx, next) => {
+		const start = new Date()
+		await next()
+		const ms = new Date() - start
+		console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+	})
+	.on('error', (error, ctx) => {
+		console.log('奇怪的错误' + JSON.stringify(ctx.onerror))
+		console.log('server error:' + error)
+	})
+
+export default app
