1edc327eaf9af7b204357e42716b760f6dd6ce16
gycdgg <1165297373@qq.com>
2018-06-25 18:27:31 +0800

    sync code

---
+++ routes/client/home.js
@@ -0,0 +1,10 @@
+
+class Home {
+  test = async (ctx) => {
+    ctx.body = {
+      code: 200
+    }
+  }
+}
+
+export default Home
--- routes/client/index.js
+++ routes/client/index.js
@@ -1,4 +1,7 @@
 import Router from 'koa-router'
+import Home from './home'
+
+const home = new Home()
 const router = Router()
 
 router.post('/login', async (ctx, next) => {
@@ -7,12 +10,8 @@ router.post('/login', async (ctx, next) => {
   }
 })
 /**
- * check login status and get user info
+ * get homepage resourse
  */
-router.get('/session', async (ctx, next) => {
-  ctx.body = {
-    text: 'welcome'
-  }
-})
+router.get('/home', home.get)
 
 export default router
