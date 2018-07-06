2bd8ee77b9caeed09a5756a2e56929093ee66e97
gycdgg <1165297373@qq.com>
2018-07-06 16:16:15 +0800

    add more mock data in init sql

--- init-mysql.js
+++ init-mysql.js
@@ -1,12 +1,15 @@
 import sequelize from './models/sequelize'
-import { Picture, User } from './models'
+import { Picture, User, Product, Feature } from './models'
+
 (async () => {
   try{
-    // drop stchemas and sync table
-    await sequelize.dropAllSchemas()
+    //drop stchemas and sync table
+    await sequelize.query('DROP TABLE `edmond`.`feature`, `edmond`.`picture`, `edmond`.`product`, `edmond`.`user`')
+    sequelize.sync({ force: true })
     await Picture.sync({ force: true })
     await User.sync({ force: true })
-
+    await Product.sync({ force: true })
+    await Feature.sync({ force: true })
     // create mock data
     await Picture.create({
       name: '22490202.jpg',
@@ -67,6 +70,43 @@ import { Picture, User } from './models'
       password: '12345',
       is_deleted: true
     })
+
+    await Product.create({
+      name: 'test product1',
+      summary: 'this is a test summary, just a test case11111',
+      pdf_name: 'name111',
+      pdf_url: 'http://www.baidu.com'
+    })
+
+    await Product.create({
+      name: 'test product2',
+      summary: 'this is a test summary, just a test case2222',
+      pdf_name: 'name222',
+      pdf_url: 'http://www.baidu.com'
+    })
+
+    await Feature.create({
+      name: 'this is a fature1',
+      productId: 1
+    })
+
+    await Feature.create({
+      name: 'this is a fature2',
+      productId: 1
+    })
+    await Feature.create({
+      name: 'this is a fature3',
+      productId: 1
+    })
+    await Feature.create({
+      name: 'this is a fature4',
+      productId: 1
+    })
+    await Feature.create({
+      name: 'this is a fature5',
+      productId: 2
+    })
+    console.log('sync mysql success')
   } catch (err) {
     console.log(err)
   }
